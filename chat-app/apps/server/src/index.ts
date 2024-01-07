f20abdf46a603c6c5b35dcd4509a25f6c55d7dcb
saikiran chary Maheshwaram <saikiranchary136@gmail.com>
2024-01-07 11:03:38 +0530

    initialized socket listeners

--- chat-app/apps/server/src/index.ts
+++ chat-app/apps/server/src/index.ts
@@ -1,13 +1,13 @@
 import http from "http";
 import SocketService from "./services/socket";
 
-
 async function init() {
   const httpServer = http.createServer();
-  const socketService =new SocketService();
+  const socketService = new SocketService();
   socketService.io.attach(httpServer);
-
+  
   const PORT = process.env.PORT ? process.env.PORT : 8000;
   httpServer.listen(PORT, () => console.log(`Server started at ${PORT}`));
+  socketService.initEventListeners();
 }
 init();
--- chat-app/apps/server/src/services/socket.ts
+++ chat-app/apps/server/src/services/socket.ts
@@ -1,13 +1,24 @@
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
-class SocketService{
-    private _io:Server;
-    constructor(){
-        this._io=new Server();
-    }
-    get io(){
-        return this._io;
-    }
+class SocketService {
+  private _io: Server;
+  constructor() {
+    console.log("init socket server");
+    this._io = new Server();
+  }
+  get io() {
+    return this._io;
+  }
+  public initEventListeners() {
+    const io = this.io;
+    console.log("initize socket listeners");
+    io.on("connect", (socket) => {
+      console.log(`New socket connected`, socket.id);
 
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("new message ", message);
+      });
+    });
+  }
 }
 export default SocketService;
